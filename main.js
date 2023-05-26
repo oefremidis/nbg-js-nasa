7be19e8227bdc2ca3197ac07bb385e522881a3ea
Odysseas Efremidis <oefremidis@athtech.gr>
2023-05-26 09:31:33 +0300

    Final commit

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
+const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
 
-document.getElementById('get').addEventListener('click', async () => {
-        
+document.getElementById("get").addEventListener("click", async () => {
+  try {
     let response = await fetch(url);
+    console.log(`OK: ${response.ok}, Status: ${response.status}`);
+
+    if (!response.ok) {
+      throw new Error("Error during fetch...");
+    }
+
     let data = await response.json();
-    
-    document.getElementById('explanation').innerHTML = data.explanation;
-    document.getElementById('photo').src = data.url;
 
+    document.getElementById("explanation").innerHTML = data.explanation;
+    const photo = document.getElementById("photo");
+    photo.src = data.url;
+    photo.style.width = '50%';
+
+
+  } catch (error) {
+    console.error(error.message);
+  }
 });
-
-
